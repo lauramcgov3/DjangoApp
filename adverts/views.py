131d147c336cdf10ebe3d0a76ee2d85e61175014
Laura <laura.mcgovern3@gmail.com>
2016-07-21 15:49:16 +0100

    Logout page working

--- adverts/views.py
+++ adverts/views.py
@@ -2,5 +2,8 @@ from django.shortcuts import render_to_response
 from django.template.context import RequestContext
 
 def home(request):
+   context = RequestContext(request,
+                           {'request': request,
+                            'user': request.user})
    return render_to_response('adverts/home.html',
-                             {})
+                             context_instance=context)
